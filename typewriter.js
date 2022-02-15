09d979ad990a8588dee27bfbd91c37bef7a64efd
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-15 23:32:42 +0000

    adding typewriter file

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+let timer = 0;
+for (const char of sentence + "\n") {
+  setTimeout(() => { 
+    process.stdout.write(char);  
+  }, timer);
+  timer += 500;
+};
+
+
